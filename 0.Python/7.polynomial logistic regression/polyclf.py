Fix: change standard deviation to range scaling in polynomial regression

0.Python/7.polynomial logistic regression/polyclf.py
@@ -7,43 +7,36 @@ class PolyClassifier:
     def __init__(self, datapoints, a=0.1, degree=2):
         #t0, t1, t2, t1^2, t2^2, t1*t2
         self.theta   = np.zeros(6)     
-        self.mu, self.std, self.X = self.normalize_data(datapoints[:,:-1], extract_stats=True, keep_lastcolumn=True)  
-        self.X = self.polynomize_data(self.X, chop_lastcolumn=False, degree=2)
+        self.X = self.polynomize_data(datapoints[:,:-1], degree=2)
+        self.mu, self.rng, self.X = self.normalize_data(self.X, extract_stats=True)
         self.y = datapoints[:,-1]
         self.a = a
 
  
-    def normalize_data(self, datapoints, extract_stats=False, keep_lastcolumn=False):
+    def normalize_data(self, datapoints, extract_stats=False):
         '''accepts prepared datapoints, with first column - all ones'''
         # mu0 = 0
         # std0 = 1
         if extract_stats:
-            if keep_lastcolumn:
-                mu_except_last = np.mean(datapoints[:,:-1], axis=0)
-                mu = np.append(mu_except_last,np.zeros(1))
-                std_except_last = np.std(datapoints[:,:-1], axis=0)
-                std = np.append(std_except_last,np.ones(1))
-            else:
-                mu = np.mean(datapoints, axis=0)
-                std = np.std(datapoints, axis=0)
+            mu_except_first = np.mean(datapoints[:,1:], axis=0)
+            mu = np.append(np.zeros(1),mu_except_first)
+            max_except_first = np.max(datapoints[:,1:], axis=0)
+            min_except_first = np.min(datapoints[:,1:], axis=0)
+            rng = np.append(np.ones(1),max_except_first-min_except_first)
         else:                        
             mu  = self.mu
-            std = self.std
+            rng = self.rng
 
-        datapoints = (datapoints - mu) / std
+        datapoints = (datapoints - mu) / rng
 
-        return mu, std, datapoints
+        return mu, rng, datapoints
 
 
-    def polynomize_data(self, datapoints, chop_lastcolumn=False, degree=2):
+    def polynomize_data(self, datapoints, degree=2):
         dp_len = datapoints.shape[0]
         assert degree == 2, 'Feature Not Implemented yet'
 
-        if chop_lastcolumn:
-            orig_features   = datapoints[:,:-1]
-        else:
-            orig_features = datapoints
-
+        orig_features = datapoints
         squred_features = orig_features**2
         feature_combinations = np.multiply(datapoints[:,0],datapoints[:,1])
         return np.column_stack((np.ones(dp_len),
@@ -65,9 +58,9 @@ class PolyClassifier:
         h = self.predict(self.X) # hypothesis vector                  
         return (self.X.transpose().dot((h - self.y))) / m
 
-    def predictor_func(self, X):
-        _, _, X = self.normalize_data(X)
+    def predictor_func(self, X):        
         X = self.polynomize_data(X)
+        _, _, X = self.normalize_data(X)
         return self.predict(X)
 
     def CostFunction(self):